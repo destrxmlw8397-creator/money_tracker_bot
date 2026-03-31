43c7665836dd9c3421fa7c37d8d976c7fe4a14a6
destrxmlw8397-creator <destrxmlw8397@gmail.com>
2026-03-31 23:55:04 +0600

    Create logger.py

---
+++ bot/utils/logger.py
@@ -0,0 +1,22 @@
+import logging
+import sys
+
+def setup_logger():
+    """Setup logging configuration"""
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    
+    # Console handler
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setLevel(logging.INFO)
+    
+    # Formatter
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+    console_handler.setFormatter(formatter)
+    
+    logger.addHandler(console_handler)
+    
+    return logger

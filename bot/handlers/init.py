65bda4511b22aa95300177c7aa64208944076632
destrxmlw8397-creator <destrxmlw8397@gmail.com>
2026-04-01 02:07:38 +0600

    Create init.py

---
+++ bot/handlers/init.py
@@ -0,0 +1,7 @@
+from .base import BaseHandler
+from .start import StartHandler
+from .balance import BalanceHandler
+from .callback import CallbackHandler
+from .message import MessageHandler
+
+__all__ = ['BaseHandler', 'StartHandler', 'BalanceHandler', 'CallbackHandler', 'MessageHandler']

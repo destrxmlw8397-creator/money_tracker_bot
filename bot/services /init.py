2439111594a8c8c7b6dc45ed743d82d1fb67107c
destrxmlw8397-creator <destrxmlw8397@gmail.com>
2026-04-01 02:45:52 +0600

    Delete bot/services  directory

--- bot/services /init.py
+++
@@ -1,8 +0,0 @@
-from .transaction_service import *
-
-__all__ = [
-    'get_user_monthly_data', 'update_user_db', 'update_wallet_only',
-    'get_lifetime_wallet_balance', 'generate_pdf',
-    'process_debt_entry_with_balance', 'process_debt_repayment',
-    'show_wallets_for_debt', 'show_wallets_for_repayment', 'show_wallets_for_out_repayment'
-]

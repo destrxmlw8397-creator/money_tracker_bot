bb42c07b09e9ce6790c5da9f7c62d204fdbc878c
destrxmlw8397-creator <destrxmlw8397@gmail.com>
2026-04-01 01:56:47 +0600

    Update start.py

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -4,16 +4,11 @@ from bot.utils.helpers import get_current_month
 from telethon import Button
 
 class StartHandler(BaseHandler):
-    """Handler for /start command"""
-    
     async def handle(self, event):
-        """Handle /start command"""
         user_id = event.sender_id
-        welcome_text = t(user_id, 'welcome', get_current_month())
-        await event.respond(welcome_text)
+        await event.respond(t(user_id, 'welcome', get_current_month(event)))
     
     async def handle_language(self, event):
-        """Handle language selection"""
         user_id = event.sender_id
         buttons = [
             [Button.inline(t(user_id, 'lang_bn'), b"lang_bn")],

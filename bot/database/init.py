8af1828b32602337027b0e6f586d861a3027d1bd
destrxmlw8397-creator <destrxmlw8397@gmail.com>
2026-04-01 09:12:03 +0600

    Update init.py

--- bot/database/init.py
+++ bot/database/init.py
@@ -1,13 +1,29 @@
+"""
+Database Module Initialization
+Exports database connection and repository classes
+"""
+
 from .connection import get_db_connection, init_db
 from .repositories import (
-    BalanceRepository, DebtRepository, DebtHistoryRepository,
-    OutstandingRepository, OutstandingHistoryRepository,
-    GoalRepository, GoalHistoryRepository, UserRepository
+    BalanceRepository,
+    DebtRepository,
+    DebtHistoryRepository,
+    OutstandingRepository,
+    OutstandingHistoryRepository,
+    GoalRepository,
+    GoalHistoryRepository,
+    UserRepository
 )
 
 __all__ = [
-    'get_db_connection', 'init_db',
-    'BalanceRepository', 'DebtRepository', 'DebtHistoryRepository',
-    'OutstandingRepository', 'OutstandingHistoryRepository',
-    'GoalRepository', 'GoalHistoryRepository', 'UserRepository'
+    'get_db_connection',
+    'init_db',
+    'BalanceRepository',
+    'DebtRepository',
+    'DebtHistoryRepository',
+    'OutstandingRepository',
+    'OutstandingHistoryRepository',
+    'GoalRepository',
+    'GoalHistoryRepository',
+    'UserRepository'
 ]

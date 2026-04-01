69ec6e0a23c3252b81fa9262fd1bdb29a68f404b
destrxmlw8397-creator <destrxmlw8397@gmail.com>
2026-04-01 11:16:58 +0600

    Update init.py

--- bot/handlers/init.py
+++ bot/handlers/init.py
@@ -1,7 +1,32 @@
+"""
+Handlers Module Initialization
+Exports all handler classes
+"""
+
 from .base import BaseHandler
 from .start import StartHandler
 from .balance import BalanceHandler
+from .report import ReportHandler
+from .history import HistoryHandler
+from .debt import DebtHandler
+from .goal import GoalHandler
+from .transfer import TransferHandler
+from .pdf import PDFHandler
+from .wallet import WalletHandler
 from .callback import CallbackHandler
 from .message import MessageHandler
 
-__all__ = ['BaseHandler', 'StartHandler', 'BalanceHandler', 'CallbackHandler', 'MessageHandler']
+__all__ = [
+    'BaseHandler',
+    'StartHandler',
+    'BalanceHandler',
+    'ReportHandler',
+    'HistoryHandler',
+    'DebtHandler',
+    'GoalHandler',
+    'TransferHandler',
+    'PDFHandler',
+    'WalletHandler',
+    'CallbackHandler',
+    'MessageHandler',
+]

98d891dcd02b0317fa3e77e395808615824e2834
destrxmlw8397-creator <destrxmlw8397@gmail.com>
2026-04-01 11:13:03 +0600

    Update init.py

--- bot/services/init.py
+++ bot/services/init.py
@@ -1,8 +1,37 @@
-from .transaction_service import *
+"""
+Services Module Initialization
+Exports all transaction service functions
+"""
+
+from .transaction_service import (
+    # User data functions
+    get_user_monthly_data,
+    update_user_db,
+    update_wallet_only,
+    get_lifetime_wallet_balance,
+    
+    # PDF generation
+    generate_pdf,
+    
+    # Debt operations
+    process_debt_entry_with_balance,
+    process_debt_repayment,
+    
+    # Wallet selection functions
+    show_wallets_for_debt,
+    show_wallets_for_repayment,
+    show_wallets_for_out_repayment,
+)
 
 __all__ = [
-    'get_user_monthly_data', 'update_user_db', 'update_wallet_only',
-    'get_lifetime_wallet_balance', 'generate_pdf',
-    'process_debt_entry_with_balance', 'process_debt_repayment',
-    'show_wallets_for_debt', 'show_wallets_for_repayment', 'show_wallets_for_out_repayment'
+    'get_user_monthly_data',
+    'update_user_db',
+    'update_wallet_only',
+    'get_lifetime_wallet_balance',
+    'generate_pdf',
+    'process_debt_entry_with_balance',
+    'process_debt_repayment',
+    'show_wallets_for_debt',
+    'show_wallets_for_repayment',
+    'show_wallets_for_out_repayment',
 ]
